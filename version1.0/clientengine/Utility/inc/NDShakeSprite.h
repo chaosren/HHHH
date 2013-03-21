ee6486459ef29093f37c5d928d411f88907e76d3
GuoHao <hubris6965@hotmail.com>
2013-03-21 19:02:25 +0800

    加了兩個空類：特別精靈效果

---
+++ version1.0/clientengine/Utility/inc/NDShakeSprite.h
@@ -0,0 +1,28 @@
+/*
+*
+*/
+
+#ifndef NDSHAKESPRITE_H
+#define NDSHAKESPRITE_H
+
+#include "define.h"
+#include "NDSprite.h"
+
+NS_NDENGINE_BGN
+
+class NDShakeSprite:public NDSprite
+{
+	DECLARE_CLASS(NDShakeSprite)
+
+public:
+
+	NDShakeSprite();
+	virtual ~NDShakeSprite();
+
+protected:
+private:
+};
+
+NS_NDENGINE_END
+
+#endif
---
+++ version1.0/clientengine/Utility/src/NDShakeSprite.cpp
@@ -0,0 +1,17 @@
+#include "NDShakeSprite.h"
+
+NS_NDENGINE_BGN
+
+IMPLEMENT_CLASS(NDShakeSprite,NDSprite)
+
+NDShakeSprite::NDShakeSprite()
+{
+
+}
+
+NDShakeSprite::~NDShakeSprite()
+{
+
+}
+
+NS_NDENGINE_END
