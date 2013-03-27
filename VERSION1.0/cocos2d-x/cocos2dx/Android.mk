73e022afeb4dd4b6e3abc12db54efc1d6c4b8e89
zhangwq <zhang_wei_qiang@sina.com>
2013-03-27 14:55:23 +0800

    编译问题

--- VERSION1.0/cocos2d-x/cocos2dx/Android.mk
+++ VERSION1.0/cocos2d-x/cocos2dx/Android.mk
@@ -25,6 +25,7 @@ actions/CCActionPageTurn3D.cpp \
 actions/CCActionProgressTimer.cpp \
 actions/CCActionTiledGrid.cpp \
 actions/CCActionTween.cpp \
+actions/CCShake.cpp \
 base_nodes/CCAtlasNode.cpp \
 base_nodes/CCNode.cpp \
 cocoa/CCAffineTransform.cpp \
