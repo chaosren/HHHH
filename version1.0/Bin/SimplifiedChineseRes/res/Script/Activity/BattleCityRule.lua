f5095bab74832a46263545efa4ecbd9ec2424a6c
tangziqin <tzq1103@126.com>
2013-09-03 18:37:13 +0800

    11111

--- version1.0/Bin/SimplifiedChineseRes/res/Script/Activity/BattleCityRule.lua
+++ version1.0/Bin/SimplifiedChineseRes/res/Script/Activity/BattleCityRule.lua
@@ -4,8 +4,8 @@ local p = BattleCityRule
 local ID_CONTAINER = 6;
 local ID_TEXT_RULE = 87;
 
-local rule_info1 = "參加條件：只有軍團成員才能參與《攻城掠地》活動。\n\n活動規則：\n  1.《攻城掠地》活動每3日重置一次，每次重置後城池為無人佔領狀態。\n  2.攻擊城池時，若守城方隊列無任何玩家駐守，將直接攻擊守城NPC，擊敗守城NPC后，城池防御度下降。\n  3.若守城方隊列有玩家駐守，攻擊方可以自行選擇目標攻擊，失敗的一方將退出戰鬥佇列並累積5分鐘CD時間內，CD時間達到20分鐘後將無法繼續參戰。（防守方規則相同）\n"
-local rule_info2 = "  4.第一個將城池防禦度降為0的玩家所屬軍團成為該城池的擁有者。同時該城池30分鐘內無法被攻擊。\n  5.佔領城池的軍團所屬玩家，將持續獲得獨有的城池佔領獎勵，直到該城池被其他軍團奪取或者活動重置。（每個軍團只能佔領一個城池）\n  6.活動周期內，軍團首次攻占城池將會獲得一次性獎勵。\n  7.防守方佔領城池的時間越久，將會獲得防守疲勞狀態，使防守難度加大。\n  8.在戰鬥佇列中連勝多次的玩家，將會得到額外的個人疲勞狀態。\n"
+local rule_info1 = "參加條件：只有軍團成員才能參與《攻城掠地》活動。\n\n活動規則：\n  1.《攻城掠地》活動每3日重置一次，每次重置後城池為無人佔領狀態。\n  2.攻擊城池時，若守城方隊列無任何玩家駐守，將直接攻擊守城NPC，擊敗守城NPC后，城池防御度下降。\n  3.若守城方隊列有玩家駐守，攻擊方可以自行選擇目標攻擊，失敗的一方將退出戰鬥隊列並累積5分鐘CD時間內，CD時間達到20分鐘後將無法繼續參戰。（防守方規則相同）\n"
+local rule_info2 = "  4.第一個將城池防禦度降為0的玩家所屬軍團成為該城池的擁有者。同時該城池30分鐘內無法被攻擊。\n  5.佔領城池的軍團所屬玩家，將持續獲得獨有的城池佔領獎勵，直到該城池被其他軍團奪取或者活動重置。（每個軍團只能佔領一個城池）\n  6.活動周期內，軍團首次攻占城池將會獲得一次性獎勵。\n  7.防守方佔領城池的時間越久，將會獲得防守疲勞狀態，使防守難度加大。\n  8.在戰鬥隊列中連勝多次的玩家，將會得到額外的個人疲勞狀態。\n"
 
 
 local ctrl_tag = {btn_close=49,txt_rule=87}
@@ -70,7 +70,7 @@ function p.InitData()
 	end
     
 	local Container = GetScrollViewContainer(layer, ID_CONTAINER);
-	local ListViewSize = CGSizeMake(230*CoordScaleX, 200*CoordScaleY);
+	local ListViewSize = CGSizeMake(230*CoordScaleX, 230*CoordScaleY);
 	Container:EnableScrollBar(true);
 	Container:SetViewSize(ListViewSize);
 	
