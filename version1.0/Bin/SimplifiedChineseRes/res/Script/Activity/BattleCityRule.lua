aeedd4ba21b3303da044f2ec65570a222527662a
tangziqin <tzq1103@126.com>
2013-09-03 16:09:31 +0800

    1.規則說明修改

--- version1.0/Bin/SimplifiedChineseRes/res/Script/Activity/BattleCityRule.lua
+++ version1.0/Bin/SimplifiedChineseRes/res/Script/Activity/BattleCityRule.lua
@@ -1,7 +1,12 @@
 BattleCityRule = {}
 local p = BattleCityRule
 
-local rule_info = "參加條件：只有軍團成員才能參與《攻城略地》活動。\n\n活動規則：\n  1.《攻城略地》活動每3日重置一次，每次重置后城池為無人占領狀態。\n  2.攻擊城池時，若守城方隊列無任何玩家駐守，將直接攻擊守城NPC，擊敗守城NPC后，城池防御度下降。\n  3.若守城方隊列有玩家駐守，攻擊方可以自行選擇目標攻擊，失敗的一方將退出戰斗隊列并累積一段CD時間內，CD時間達到30分鐘后將無法繼續參戰。（防守方規則相同）\n  4.第一個將城池防御度降為0的玩家所屬軍團成為該城池的擁有者。同時該城池半小時內無法被攻擊。\n  5.占領城池的軍團所屬玩家，將持續獲得占領獎勵，直到城池被其他軍團奪取或者活動重置。（每個軍團只能占領一個城池）\n  6.活動周期內，軍團首次攻占城池將會獲得一次性獎勵。\n  7.占領城池的時間越久，將會累積防守疲勞狀態，使防守難度加大。\n  8.在戰斗隊列中連勝多次的玩家，將會得到額外的個人疲勞狀態。\n"
+local ID_CONTAINER = 6;
+local ID_TEXT_RULE = 87;
+
+local rule_info1 = "參加條件：只有軍團成員才能參與《攻城掠地》活動。\n\n活動規則：\n  1.《攻城掠地》活動每3日重置一次，每次重置後城池為無人佔領狀態。\n  2.攻擊城池時，若守城方隊列無任何玩家駐守，將直接攻擊守城NPC，擊敗守城NPC后，城池防御度下降。\n  3.若守城方隊列有玩家駐守，攻擊方可以自行選擇目標攻擊，失敗的一方將退出戰鬥佇列並累積5分鐘CD時間內，CD時間達到20分鐘後將無法繼續參戰。（防守方規則相同）\n"
+local rule_info2 = "  4.第一個將城池防禦度降為0的玩家所屬軍團成為該城池的擁有者。同時該城池30分鐘內無法被攻擊。\n  5.佔領城池的軍團所屬玩家，將持續獲得獨有的城池佔領獎勵，直到該城池被其他軍團奪取或者活動重置。（每個軍團只能佔領一個城池）\n  6.活動周期內，軍團首次攻占城池將會獲得一次性獎勵。\n  7.防守方佔領城池的時間越久，將會獲得防守疲勞狀態，使防守難度加大。\n  8.在戰鬥佇列中連勝多次的玩家，將會得到額外的個人疲勞狀態。\n"
+
 
 local ctrl_tag = {btn_close=49,txt_rule=87}
 local function GetParent()
@@ -42,9 +47,48 @@ function p.LoadUI()
 		return false;
 	end
 	uiLoad:Load("city_battle/city_battle_rule.ini", layer, p.OnUIEvent, 0, 0);
-	local label_rule = GetLabel(layer,ctrl_tag.txt_rule)
-	label_rule:SetText(rule_info)
+	
+	p.InitData();
+	
+	--local label_rule = GetLabel(layer,ctrl_tag.txt_rule)
+	--label_rule:SetText(rule_info)
+	
 	return true
+end
+
+
+function p.InitData()
+
+	local scene = GetSMGameScene();
+	if nil == scene then
+		return nil;
+	end
+	
+	local layer = GetUiLayer(scene, NMAINSCENECHILDTAG.SG7x7RuleUI);
+	if nil == layer then
+		return nil;
+	end
+    
+	local Container = GetScrollViewContainer(layer, ID_CONTAINER);
+	local ListViewSize = CGSizeMake(230*CoordScaleX, 185*CoordScaleY);
+	Container:EnableScrollBar(true);
+	Container:SetViewSize(ListViewSize);
+	
+	for i = 1, 2 do
+		local view = createUIScrollView();
+		view:Init(false);
+		view:SetViewId(i);
+		view:SetTag(i);  
+		
+		local viewText = nil;
+		if i == 1 then
+			viewText = CreateLabel(rule_info1, CGRectMake(0, 0, ListViewSize.w , ListViewSize.h), 12, ccc4(255,255,255,255));
+		else
+			viewText = CreateLabel(rule_info2, CGRectMake(0, 0, ListViewSize.w , ListViewSize.h), 12, ccc4(255,255,255,255));
+		end
+		view:AddChildZ(viewText, 2);
+		Container:AddView(view);
+	end
 end
 
 function p.OnUIEvent(uiNode, uEventType, param)
